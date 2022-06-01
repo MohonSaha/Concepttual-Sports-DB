143e8d998fc55a9f1da72939cadbd23d44efb075
MohonSaha <practicewithmohon@gmail.com>
2022-06-01 13:56:37 +0600

    add code

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+
+
 const allPlayers = () =>{
     // console.log("coool");
     const searchValue = document.getElementById("search-box");
+    document.getElementById("spinner").style.display = "block";
     const searchText = searchValue.value;
     searchValue.value = '';
     // console.log(searchText);
 
+    //===================== LOAD DATA ========================
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data =>  showPlayerDetails(data.player));
 }
 
+    //======================= DISPLAY DATA =======================
 const showPlayerDetails = players => {
     const parent = document.getElementById("player-container");
     parent.textContent = '';
+
+    if(players){
+        document.getElementById("spinner").style.display = "none";
+    }
+    else{
+        document.getElementById("spinner").style.display = "block";
+        parent.textContent = '';
+    }
+
+    
+    // document.getElementById("spinner").style.display = "block";
     players.forEach(player => {
         // console.log(player);
         const div = document.createElement("div");
