a11d9e194ec49c98d7676729012ad98615373e96
MohonSaha <practicewithmohon@gmail.com>
2022-06-01 13:00:00 +0600

    add image

--- app.js
+++ app.js
@@ -42,5 +42,36 @@ const details = (id) => {
     const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => setDetails(data.players[0]));
+}
+
+const setDetails = info =>{
+    console.log(info);
+
+    if(info.strGender == "Male"){
+        document.getElementById("male").style.display ="block";
+        document.getElementById("female").style.display ="none";
+    }
+    else{
+        document.getElementById("male").style.display ="none";
+        document.getElementById("female").style.display ="block";
+    }
+
+    const displayDetails= document.getElementById("details-container");
+    displayDetails.textContent ='';
+    const div = document.createElement("div")
+    div.innerHTML = `
+                <img class="w-50 mt-3" src="${info.strThumb}" alt="">
+                <h3 class="text-info">Name: ${info.strPlayer}</h3>
+                <h3>Height: ${info.strHeight}</h3>
+                <h4>Bath: ${info.strBirthLocation}</h4>
+                <h4>Country: ${info.strNationality}</h4>
+                <h4>Sport: ${info.strSport}</h4>
+                <h4>Team: ${info.strTeam}</h4>
+                <p> About: ${info.strDescriptionEN} </p>
+                
+
+
+    `
+    displayDetails.appendChild(div);
 }
